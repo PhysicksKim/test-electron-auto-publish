f8102e99817ebd365bff3341791b1a35f577bc71
PhysicksKim <physickskim@gmail.com>
2024-06-04 02:08:47 +0900

    v0.0.6 delta auto update test

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -13,7 +13,7 @@ function Hello() {
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <h3>DeltaUpdate added Version v0.0.5 !!!</h3>
+      <h3>DeltaUpdate Auto Update test Version v0.0.6 !!!🎇🎇✨✨💎💍</h3>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
