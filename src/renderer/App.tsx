3d1b072126e7736f5bcf20305bc6f9b63a8e0d83
PhysicksKim <physickskim@gmail.com>
2024-06-03 22:46:00 +0900

    v0.0.2 new release test

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -13,6 +13,7 @@ function Hello() {
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
+      <h3>New Version v0.0.2 !!!!</h3>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
